1d8705f7020ed5c6f32201959b6b6824b6b90160
Yuri Shemetov <yuri-sh@tut.by>
2022-01-26 18:31:31 +0300

    v.1.2 - Added Top5 and Random

--- src/images/urls.py
+++ src/images/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import ImageViewSet
+from .views import ImageViewSet, Top5ViewSet, RandomViewSet
 
 # Создаем router и регистрируем ViewSet
 router = DefaultRouter()
-router.register(r'images', ImageViewSet)
+router.register(r'images', ImageViewSet, basename='images')
+router.register(r'top5', Top5ViewSet, basename='top5')
+router.register(r'random', RandomViewSet, basename='random')
 # URLs настраиваются автоматически роутером
 urlpatterns = router.urls
--- src/images/views.py
+++ src/images/views.py
@@ -3,9 +3,20 @@ from rest_framework import permissions
 from .models import Image
 from .serializers import ImageSerializer
 from likes.mixins import LikedMixin, ReportedMixin
+from django.db.models import Count, Max
+import random
 
 class ImageViewSet(LikedMixin, ReportedMixin, viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    #permission_classes = (permissions.AllowAny,)
+
+class Top5ViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all().annotate(cnt=Count('likes')).order_by('-cnt')[0:5]
+    serializer_class = ImageSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class RandomViewSet(LikedMixin, ReportedMixin, viewsets.ModelViewSet):
+    queryset = Image.objects.alias(cnt=Count('reports')).filter(cnt__lt=1).order_by('?')
+    serializer_class = ImageSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
