66c94ec16778947a21fc1c02a1f2f0a5914cbe62
Yuri Shemetov <yuri-sh@tut.by>
2022-01-28 20:00:31 +0300

    v.1.3 - Updated Random & Url for Media

--- src/images/views.py
+++ src/images/views.py
@@ -1,9 +1,8 @@
-from rest_framework import viewsets
-from rest_framework import permissions 
+from rest_framework import viewsets, permissions
 from .models import Image
 from .serializers import ImageSerializer
 from likes.mixins import LikedMixin, ReportedMixin
-from django.db.models import Count, Max
+from django.db.models import Count
 import random
 
 class ImageViewSet(LikedMixin, ReportedMixin, viewsets.ModelViewSet):
@@ -17,6 +16,11 @@ class Top5ViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class RandomViewSet(LikedMixin, ReportedMixin, viewsets.ModelViewSet):
-    queryset = Image.objects.alias(cnt=Count('reports')).filter(cnt__lt=1).order_by('?')
+    list_id = [i.id for i in Image.objects.alias(cnt=Count('reports')).filter(cnt__lt=1)]
+    random.shuffle(list_id)
+    clauses = ' '.join(['WHEN id=%s THEN %s' % (pk, i) for i, pk in enumerate(list_id)])
+    ordering = 'CASE %s END' % clauses
+    queryset = Image.objects.filter(pk__in=list_id).extra(
+        select={'ordering': ordering}, order_by=('ordering',))
     serializer_class = ImageSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
